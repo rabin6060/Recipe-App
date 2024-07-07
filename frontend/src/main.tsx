e32e6889c9de2e0da8d8dfea6ff4a6b17676aca1
Anand Mainali <anand.mainali@cyberarrow.io>
2024-07-07 10:36:59 +0545

    socket config for all component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from './Context/UserContext.tsx'
-import { RecipeProvider } from './Context/RecipeContext.tsx'
-import { NotificationProvider } from './Context/Notification.tsx'
-//import { ThemeProvider } from './components/theme-provider.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider, useUser } from './Context/UserContext.tsx'; // Adjust the import path if necessary
+import { RecipeProvider } from './Context/RecipeContext.tsx';
+import { NotificationProvider } from './Context/Notification.tsx';
+
+const Root: React.FC = () => {
+  const { user } = useUser(); 
+  const userId = user ? user.data._id : ''; 
+  const url = "http://localhost:7000"
+
+  return (
+    <NotificationProvider url={url} userId={userId}>
+      <App />
+    </NotificationProvider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-     <UserProvider>
-      <RecipeProvider>
-        <NotificationProvider>
-          <App />
-        </NotificationProvider>
-      </RecipeProvider>
-     </UserProvider>
+      <UserProvider>
+        <RecipeProvider>
+          <Root />
+        </RecipeProvider>
+      </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
