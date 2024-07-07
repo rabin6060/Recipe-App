5a99e85842f5e5172258e576f3a5c0bd076570d4
Anand Mainali <anand.mainali@cyberarrow.io>
2024-07-07 10:37:56 +0545

    some cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,31 +13,8 @@ import Profile from './pages/Profile'
 import EditRecipe from './pages/EditRecipe'
 import { Toaster } from './components/ui/sonner'
 import Protected from './components/ProtectedRoute'
-import { useEffect } from 'react'
-import { get } from './api/notification'
-import { useNotification } from './Context/Notification'
-
-
-
 
 function App() {
-  // const {notifications,setNotifications} = useNotification()
-  // useEffect(()=>{
-  //   const fetch = async () =>{
-  //     try {
-        
-  //       const res =await get()
-  //       console.log(res && res.data.data)
-  //       res && setNotifications(res?.data?.data)
-        
-  //     } catch (error) {
-  //       console.log(error)
-  //     }
-  //   }
-  //   fetch()
-  // },[])
-  // console.log(notifications)
-  
   return (
     <div className="dark:bg-black">
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
